9484e39795dfd9032add21a05a0d3d509bd7e4a7
Jan Baer <jan.baer@entiac.com>
2022-04-27 16:02:28 +0200

    just created empty Class

---
+++ materialscanner/WhiteBalance.cpp
@@ -0,0 +1,23 @@
+
+#include "WhiteBalance.h"
+using namespace cv;
+
+
+class MyClass
+{
+public:
+	MyClass();
+	~MyClass();
+
+private:
+
+};
+
+MyClass::MyClass()
+{
+	cv::Mat benchmarkWhite = imread("C:\stud\Dev\Im\Bl.jpg");
+}
+
+MyClass::~MyClass()
+{
+}
---
+++ materialscanner/WhiteBalance.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include"GalaxyIncludes.h"
+#include<QWidget>
+#include<QImage>
+#include<QObject>
+#include "opencv2/opencv.hpp"
+#include "opencv2/core/mat.hpp"
+#include "CaptureEventHandler.h"
